bd2d60a3fb1e25832f2a8264c8dcc7179d608059
TsaybelSA <tsaybelsa@gmail.com>
2022-05-14 09:35:56 +0600

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Сергей Цайбель on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
