23dff185fe26b97fb0d2e6e1901229273f9e8067
TsaybelSA <tsaybelsa@gmail.com>
2022-05-15 20:32:36 +0600

    Created location struct. Made main view.

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -4,14 +4,62 @@
 //
 //  Created by Сергей Цайбель on 13.05.2022.
 //
-
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+	
+	@State private var showingDetails = false
+	
+	@State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 52.31, longitude: 13.24), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+	
+	@State private var locations = [Location]()
+	
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+		ZStack {
+			Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+				MapAnnotation(coordinate: location.coordinates) {
+					VStack {
+						Image(systemName: "star.circle")
+							.resizable()
+							.foregroundColor(.red)
+							.frame(width: 44, height: 44)
+							.background(.white)
+							.clipShape(Circle())
+						
+						Text(location.name)
+					}
+				}
+			}
+			.ignoresSafeArea()
+			
+			Circle()
+				.fill(.blue)
+				.opacity(0.3)
+				.frame(width: 44, height: 44)
+			
+			VStack {
+				Spacer()
+				HStack {
+					Spacer()
+					Button {
+						let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
+						locations.append(newLocation)
+					} label: {
+						Image(systemName: "plus")
+					}
+					.padding()
+					.background(.black.opacity(0.7))
+					.foregroundColor(.white)
+					.font(.title)
+					.clipShape(Circle())
+					.padding(.trailing)
+				}
+			}
+		}
+				
+	}
+
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ BucketList/Location.swift
@@ -0,0 +1,27 @@
+//
+//  Location.swift
+//  BucketList
+//
+//  Created by Сергей Цайбель on 15.05.2022.
+//
+
+import CoreLocation
+import SwiftUI
+
+struct Location: Identifiable, Codable, Equatable {
+	let id: UUID
+	var name: String
+	var description: String
+	let latitude: Double
+	let longitude: Double
+	
+	var coordinates: CLLocationCoordinate2D {
+		CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+	}
+	
+	static let example = Location(id: UUID(), name: "Buckingham Palace", description: "Paul Hudson wrote here about Queen Elizabeth and her dorgis", latitude: 51.501, longitude: -0.141)
+	
+	static func ==(lhs: Location, rhs: Location) -> Bool {
+		lhs.id == lhs.id
+	}
+}
